9a3ca528bcd88d672d7fd459435e0a2c3d151db6
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-02-24 23:55:54 -0500

    backend photos read

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 const app = express();
 import { v4 as uuidv4 } from "uuid";
 import tags from './routes/tags.js';
+import photos from "./routes/photos.js"
 const port = process.env.PORT || process.argv[2] || 8080;
 const { CORS_ORIGIN } = process.env;
 app.use(cors({ origin: CORS_ORIGIN }));
@@ -19,6 +20,11 @@ app.use(express.json());
   //   res.json(tags);
   // });
 app.get("/tags", tags);
+
+// GET endpoint for all photos
+app.get("/photos", photos);
+app.get("/photos/:id", photos);
+app.get("/photos/:id/comments", photos);
 
 
 app.listen(port, () => console.log(`Listening on ${port}`));
--- routes/photos.js
+++ routes/photos.js
@@ -1,15 +1,37 @@
-// import express from 'express';
-// import fs from 'fs'
-// import crypto from 'crypto';
-// // import { getPhotos , getPhotoById , getPhotoComments } from '../data/photos.js';
+import express from 'express';
+import fs from 'fs'
+import crypto from 'crypto';
 
-// function displayPhotos() {
-// 	const photosData = fs.readFileSync("./data/photos.json");
-// 	const parsedData = JSON.parse(photosData);
-// 	return parsedData;
-// }
+const router = express.Router()
 
-// // GET endpoint for all photos
-// router.get("/", getPhotos);
-// router.get("/photos/:id ", getPhotoById);
-// router.get("/photos/:id/comments", getPhotoComments);
+function readPhotos() {
+	const photosData = fs.readFileSync("./data/photos.json");
+	const parsedData = JSON.parse(photosData);
+	return parsedData;
+}
+
+function getPhotoById (id) {
+	const photos = readPhotos();
+	return photos.find((photo) => photo.id === id);
+}
+
+
+// GET endpoint for photos
+router.get('/photos', (req, res) => {
+	const photos = readPhotos();
+	res.json(photos);
+  });
+
+router.get("/photos/:id" , (req, res) => {
+	const id = req.params.id;
+	const photo = getPhotoById(id);
+	res.json(photo);
+});
+
+router.get("/photos/:id/comments" , (req, res) => {
+	const id = req.params.id;
+	const photo = getPhotoById(id);
+	res.json(photo.comments);
+});
+
+export default router;
