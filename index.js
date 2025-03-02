cefe5b6d0e60de3d220f6b4e7a112350dddfdf36
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-02 16:16:56 -0500

    sprint-3 final

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ import cors from "cors";
 import "dotenv/config";
 const app = express();
 import { v4 as uuidv4 } from "uuid";
-import tags from './routes/tags.js';
-import photos from "./routes/photos.js"
+import tags from "./routes/tags.js";
+import photos from "./routes/photos.js";
 const port = process.env.PORT || process.argv[2] || 8080;
 const { CORS_ORIGIN } = process.env;
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(cors());
-// const express = require('express');
-// const cors = require('cors');
 
 app.use(express.json());
 
@@ -24,6 +22,5 @@ app.get("/photos", photos);
 app.get("/photos/:id", photos);
 app.get("/photos/:id/comments", photos);
 app.post("/photos/:id/comments", photos);
-
 
 app.listen(port, () => console.log(`Listening on ${port}`));
